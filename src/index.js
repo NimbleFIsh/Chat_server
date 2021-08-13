1555eab79c537225c292c81f669bbbae220d9079
NimbleFIsh <karpov2001ura@mail.ru>
2021-08-13 13:01:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ function exit() {
     });
 }
 
-['SIGQUIT', 'SIGSTOP', 'SIGTERM', 'SIGUSR2', 'SIGINT'].forEach(died => 
+['SIGQUIT', 'SIGSTOP', 'SIGUSR2', 'SIGINT'].forEach(died => 
     process.on(died, exit));
 
 server.listen(process.env.port || PORT, () =>
